2449be53384dc53bf23bcb6ad69a0bbe5030128d
Dolev Hadar <dolevh@wix.com>
2021-09-11 15:04:51 +0300

    generate more sensible word lengths (2-6 letters)

---
+++ src/constants/practiceAndReviewLetterSets.ts
@@ -0,0 +1,19 @@
+import {LessonNewLetters} from '../utils/generateLetterExercises';
+
+export const letterLearningOrder: LessonNewLetters[] = [
+  ['כ', 'ח'],
+  ['ג', 'ל'],
+  ['ד', 'ך'],
+  ['ש', 'ף'],
+  ['ע', 'י'],
+  ['ר', 'ו'],
+  ['ק', 'ן'],
+  ['ם', "'"],
+  ['/', 'ט'],
+  ['פ', 'א'],
+  ['ה', 'צ'],
+  ['ב', 'ת'],
+  ['ס', 'ץ'],
+  ['ז', '.'],
+  ['נ', 'מ'],
+];
--- src/utils/generateLetterExercises.ts
+++ src/utils/generateLetterExercises.ts
@@ -1,61 +1,12 @@
+import {letterLearningOrder} from '../constants/practiceAndReviewLetterSets';
+
 const LESSON_LINE_LENGTH = 25;
 const LESSON_NUM_LINES = 3;
+const AVERAGE_WORD_LENGTH = 5;
 
 export type Letter = string;
 export type LessonNewLetters = [Letter, Letter];
 export type ExerciseText = string[];
-
-const letterLearningOrder: LessonNewLetters[] = [
-  ['כ', 'ח'],
-  ['ג', 'ל'],
-  ['ד', 'ך'],
-  ['ש', 'ף'],
-  ['ע', 'י'],
-  ['ר', 'ו'],
-  ['ק', 'ן'],
-  ['ם', "'"],
-  ['/', 'ט'],
-  ['פ', 'א'],
-  ['ה', 'צ'],
-  ['ב', 'ת'],
-  ['ס', 'ץ'],
-  ['ז', '.'],
-  ['נ', 'מ'],
-];
-
-const getRandomLetter = (lettersSet: Letter[]): Letter | ' ' => {
-  const index = Math.floor(Math.random() * (lettersSet.length + 1));
-  return [...lettersSet, ' '][index];
-};
-
-const getEmptyLinesArray = (): undefined[] => {
-  return new Array(LESSON_NUM_LINES).fill(undefined);
-};
-
-const fillLinesWithRandomLetters = (letters: Letter[]): string => {
-  return new Array(LESSON_LINE_LENGTH)
-    .fill(undefined)
-    .map(_letter => getRandomLetter(letters))
-    .join('')
-    .replace(/\s+/g, ' ')
-    .trim();
-};
-
-export const generateLetterReviewExercise = (
-  lessonLetters: LessonNewLetters,
-): ExerciseText => {
-  return getEmptyLinesArray().map(_line =>
-    fillLinesWithRandomLetters(lessonLetters),
-  );
-};
-
-// Practice lesson should contain all the letters you've learned so far
-export const generateLetterPracticeExercise = (
-  exerciseNumber: number,
-): ExerciseText => {
-  const letters = letterLearningOrder.slice(0, exerciseNumber + 1).flat();
-  return getEmptyLinesArray().map(_line => fillLinesWithRandomLetters(letters));
-};
 
 enum ExerciseType {
   REVIEW = 'review',
@@ -65,6 +16,48 @@ enum ExerciseType {
 type Exercise = {
   text: ExerciseText;
   type: ExerciseType;
+};
+
+const generateRandomWordLength = () => {
+  return Math.max(2, Math.floor(Math.random() * AVERAGE_WORD_LENGTH + 1));
+};
+
+const generateRandomLetter = (lettersSet: Letter[]): Letter | ' ' => {
+  const index = Math.floor(Math.random() * (lettersSet.length + 1));
+  return lettersSet[index];
+};
+
+const generateRandomLine = (lettersSet: Letter[]) => {
+  const numSpaces = LESSON_LINE_LENGTH / AVERAGE_WORD_LENGTH;
+  const wordLengths = new Array(numSpaces)
+    .fill(undefined)
+    .map(generateRandomWordLength);
+  const randomWords = wordLengths
+    .map(length =>
+      new Array(length + 1)
+        .fill(undefined)
+        .map(() => generateRandomLetter(lettersSet))
+        .join(''),
+    )
+    .flat();
+  return randomWords.join(' ');
+};
+
+const getEmptyLinesArray = (): undefined[] => {
+  return new Array(LESSON_NUM_LINES).fill(undefined);
+};
+
+export const generateLetterReviewExercise = (
+  lessonLetters: LessonNewLetters,
+): ExerciseText => {
+  return getEmptyLinesArray().map(_line => generateRandomLine(lessonLetters));
+};
+
+export const generateLetterPracticeExercise = (
+  exerciseNumber: number,
+): ExerciseText => {
+  const letters = letterLearningOrder.slice(0, exerciseNumber + 1).flat();
+  return getEmptyLinesArray().map(_line => generateRandomLine(letters));
 };
 
 export const getFullListOfPracticeAndReviewExercises = (): Exercise[] => {
